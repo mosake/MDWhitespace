26cf50f4848d86ddfee48dfd9f552bdacca0d82f
mosake <kellysammo@gmail.com>
2019-09-05 02:06:36 -0400

    Update MDWJs.js
    Tested with correct output

--- MDWJs.js
+++ MDWJs.js
@@ -1,13 +1,18 @@
 
 var SPC = [" ","&nbsp;","&ensp;","&ensp;[SPACE]","&emsp;","&emsp;[SPACE]"]
 function run() {
+	// since the entire input block is taken in at once, line by line is unnessesary
+	// note, replace only gets the first instance, use REGEX
 	var input = document.getElementById("userIn").value;
-	input = input.replace("     ", SPC[5])
-    input = input.replace("    ", SPC[4])
-    input = input.replace("   ", SPC[3])
-    input = input.replace("  ", SPC[2])
-    input = input.replace("/n ", "/n" + SPC[1])
+	if (input[0] == " "){
+        input = input.replace(" ", SPC[1])
+	}
+	input = input.replace(/     /g, SPC[5])
+    input = input.replace(/    /g, SPC[4])
+    input = input.replace(/   /g, SPC[3])
+    input = input.replace(/  /g, SPC[2])
+    input = input.replace(/\n /g, "\n" + SPC[1])
     // replace placeholder spaces with actual space
-    input = input.replace("[SPACE]", SPC[0])
+    input = input.replace(/\[SPACE\]/g, SPC[0])
    	document.getElementById("userOut").value = input;
 }
