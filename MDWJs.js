bb6e2278821cfabd9df732da85fbd815d18a1c5a
mosake <kellysammo@gmail.com>
2019-09-05 01:36:15 -0400

    Update MDWJs.js
    to be tested

--- MDWJs.js
+++ MDWJs.js
@@ -1,5 +1,13 @@
+
+var SPC = [" ","&nbsp;","&ensp;","&ensp;[SPACE]","&emsp;","&emsp;[SPACE]"]
 function run() {
 	var input = document.getElementById("userIn").value;
-	alert(input);
-   	document.getElementById("userOut").value = "boo!";
+	input = input.replace("     ", SPC[5])
+    input = input.replace("    ", SPC[4])
+    input = input.replace("   ", SPC[3])
+    input = input.replace("  ", SPC[2])
+    input = input.replace("/n ", "/n" + SPC[1])
+    // replace placeholder spaces with actual space
+    input = input.replace("[SPACE]", SPC[0])
+   	document.getElementById("userOut").value = input;
 }
