43952fbf9b02173a39c3b2b0b72539c578f12381
mosake <kellysammo@gmail.com>
2019-09-05 03:16:23 -0400

    Update MDWJs.js
    Set up reverse functionality

--- MDWJs.js
+++ MDWJs.js
@@ -4,6 +4,15 @@ function run() {
 	// since the entire input block is taken in at once, line by line is unnessesary
 	// note, replace only gets the first instance, use REGEX
 	var input = document.getElementById("userIn").value;
+	if ((input == "") && (document.getElementById("userOut").value != "")){
+		document.getElementById("userIn").value = mdToTxt(document.getElementById("userOut").value);
+	}
+	else {
+		document.getElementById("userOut").value = optMd(input);
+	}
+}
+
+function optMd(input) {
 	if (input[0] == " "){
         input = input.replace(" ", SPC[1])
 	}
@@ -14,5 +23,13 @@ function run() {
     input = input.replace(/\n /g, "\n" + SPC[1])
     // replace placeholder spaces with actual space
     input = input.replace(/\[SPACE\]/g, SPC[0])
-   	document.getElementById("userOut").value = input;
+    return(input);
+}
+
+function mdToTxt(input) {
+	for (var i = 1; i < 5; i = i*2) {
+		var re = new RegExp(SPC[i], "g");
+	    input = input.replace(re, " ".repeat(i));
+	}
+    return(input);
 }
